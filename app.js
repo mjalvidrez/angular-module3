fa21cb293f161ff5a768780cceef99b92b6f610a
Matthew Alvidrez <mateo75602@gmail.com>
2023-04-05 19:02:03 -0500

    finishing module 3 assignment

--- app.js
+++ app.js
@@ -3,40 +3,58 @@
 
     angular.module('NarrowItDownApp', [])
     .controller('NarrowItDownController', NarrowItDownController)
-    .service('MenuSearchService', MenuSearchService);
+    .service('MenuSearchService', MenuSearchService)
+    .directive('foundItems', foundItems);
 
 
-    // The NarrowItDownController should be injected with the 
-    // MenuSearchService. The controller should call the 
-    // getMatchedMenuItems method when appropriate and store the result 
-    // in a property called found attached to the controller instance
+
     NarrowItDownController.$inject = ['MenuSearchService'];
     function NarrowItDownController(MenuSearchService) {
-
+        // this is where we will be calling functions from the ctrl
+        var ctrl = this;
+        ctrl.finder = function() {
+            ctrl.find = MenuSearchService.getMatchedMenuItems();
+        }
     }
-
-
-    // Declare and create MenuSearchService. The service should have the 
-    // following method: getMatchedMenuItems(searchTerm). That method will 
-    // be responsible for reaching out to the server (using the $http 
-    // service) to retrieve the list of all the menu items. Once it 
-    // gets all the menu items, it should loop through them to pick out 
-    // the ones whose description matches the searchTerm. Once a list of 
-    // found items is compiled, it should return that list (wrapped in a 
-    // promise). Remember that the then function itself returns a promise. 
-    // Your method would roughly look like this:
+ 
     MenuSearchService.$inject = ['http']
-    function MenuSearchService('http') {
+    function MenuSearchService($http) {
         var service = this;
+
+        service.getMatchedMenuItems = function (searchTerm) {
+            // need to see if the term we are searching is null or empty 
+            if (searchTerm === null || searchTerm === undefined) {
+                searchTerm = " ";
+            }
+            return $http({
+                method : "GET",
+                url : ("https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json")
+            })
+            .then(function (result) {
+                var foundItems = result.data;
+                var myitems = [];
+                // this is where we need to search through the list to get what we need 
+                for (temp in foundItems) {
+                    for (item in foundItems[temp]) {
+                        // do something so you are able to get the searched term and put it into a list 
+                    }
+                }
+    
+                return myitems;
+            });
+        }
     }
 
-
-    service.getMatchedMenuItems = function (searchTerm) {
-
-        return $http(...).then(function (result) {
-
-        });
+    function foundItems() {
+        
+        var ddo = {
+            restrict : 'E',
+            scope: {
+                item: "=foundItem",
+                arrayName: '@'
+            }
+        };
+        return ddo;
     }
     
-
 })();
