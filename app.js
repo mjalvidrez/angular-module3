4340abfd94f3c2c5308fcbdd6de544c987b69139
Matthew Alvidrez <mateo75602@gmail.com>
2023-04-05 22:41:29 -0500

    finishing module 3 assignment

--- app.js
+++ app.js
@@ -14,6 +14,7 @@
         var ctrl = this;
         ctrl.finder = function() {
             ctrl.find = MenuSearchService.getMatchedMenuItems();
+            ctrl.items = something ; 
         }
     }
  
